6c1c62f5d7e21eb31a374683b64c0b3404f60fcc
ramsawh <r.baseballpower1@gmail.com>
2023-04-23 23:03:59 -0400

    pt3

---
+++ backend/SQL/test.py
@@ -0,0 +1,17 @@
+import os
+
+from sql_interaction import SQL_Interaction
+
+if __name__ == "__main__":
+    if(os.path.exists("./backend/SQL/emr_database.db")):
+        database = SQL_Interaction("./backend/SQL/emr_database.db")
+        database.create_connection()
+        print("Connecting to an existing database..")
+    else:
+        database = SQL_Interaction("./backend/SQL/emr_database.db")
+        database.create_connection()
+        database.create_tables()
+        print("Database had to be created..")
+
+    response = database.perform_insert("clients",['first_name','last_name','date_of_birth','sex'],['John','Smith', '2022-12-24', 'Male'])
+    print(f"Response: {response['last_id']}")
